055c7d74bf8a705ecfef97b2d310030890316d72
jhayesnv <jhayesdev@icloud.com>
2024-02-23 21:25:30 -0800

    Create quiz project skeleton with CRA.

---
+++ quiz/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+
+    </div>
+  )
+}
+
+export default App;
