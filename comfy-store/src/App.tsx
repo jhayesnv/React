97eea90d584fd228bf36a0225978439a1184a21c
jhayesnv <jhayesdev@icloud.com>
2024-06-04 14:15:16 -0700

    Add tailwind and Shadcn support

--- comfy-store/src/App.tsx
+++ comfy-store/src/App.tsx
@@ -1,9 +1,14 @@
+import { Button } from "./components/ui/button"
+
 function App() {
 
   return (
     <>
       <div>
-        <p>Hello World</p>
+        <h1 className="text-7xl front-bold">Hello World</h1>
+        <Button variant='destructive' size='lg' onClick={() => console.log('it works')}>
+          Click me
+        </Button>
       </div>
     </>
   )
