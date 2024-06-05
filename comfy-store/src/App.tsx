dbd82cf7f13efaf64f9bfbc5f3f0740db20ebc09
jhayesnv <jhayesdev@icloud.com>
2024-06-05 10:07:34 -0700

    Configure redux store and verify setup is functional

--- comfy-store/src/App.tsx
+++ comfy-store/src/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from "./components/ui/button"
+import { useAppSelector } from "./hooks"
 
 function App() {
+  const { name } = useAppSelector((state) => state.userState)
 
   return (
     <>
       <div>
-        <h1 className="text-7xl front-bold">Hello World</h1>
+        <h1 className="text-7xl front-bold">Hello {name}</h1>
         <Button variant='destructive' size='lg' onClick={() => console.log('it works')}>
           Click me
         </Button>
